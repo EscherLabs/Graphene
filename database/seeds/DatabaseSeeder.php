b821384ee66e4c845bd0e5ca2fe5b1689c371156
timcortesi <tim@spacebook.local>
2017-01-26 19:38:32 -0500

    Add App Definition to Database Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -40,13 +40,92 @@ class DatabaseSeeder extends Seeder
         $app = new \App\App;
         $app->name = 'My App';
         $app->site_id = $site->id;
+        $app->code = json_encode([
+            "css" => "",
+            "options" => "",
+            "scripts" => [
+            [
+                "name" => "main",
+                "content" => "
+                this.data.counter = 1;\n
+                this.data.echo = {};\n
+                this.callback = function() {\n
+                    this.app.on('hello',function(){\n
+                        console.log('stuff');\n
+                    })\n
+                    this.app.click('.btn-info', function() {\n
+                        this.app.refresh();\n
+                    })\n
+                    this.app.click('.btn-danger', function() {\n
+                        this.data.counter++;\n
+                        this.app.update();\n
+                    })\n
+                    this.app.click('.btn-warning',function() {\n
+                        this.app.get('whoami',{},function(response){\n
+                            this.data.whoami = response;\n
+                            this.app.update();\n
+                        })\n
+                    })\n
+                    this.app.click('.post-echo',function() {\n
+                        this.app.post('echo',{'result':this.".'$'."el.find('#post-echo-input').val()},function(response){\n
+                            this.data.echo.POST = response.POST;\n
+                            this.app.update();\n
+                        })\n
+                    })\n
+                    this.app.click('.get-echo',function() {\n
+                        this.app.get('echo',{'result':this.".'$'."el.find('#get-echo-input').val()},function(response){\n
+                            this.app.update({echo:{GET:response.GET}});\n
+                        })\n
+                    })\n
+                    this.app.click('.updateDirect', function(){\n
+                        this.data.counter = 100;//.set({counter:100});\n
+                        this.app.redraw();\n
+                        \n
+                    })\n
+                }
+                ",
+            ],
+            ],
+            "sources" => [
+                ["name" => "whoami",],
+                ["name" => "echo",],
+            ],
+            "templates" => [
+            [
+                "name" => "main",
+                "content" => "
+                <div class='btn btn-info'>Refresh Button!</div>\n
+                <div>{{options.mytext}}</div>\n
+                <div class='btn btn-danger'>Redraw Increment Counter</div>\n
+                <div><strong>Counter:</strong> {{counter}}</div>\n
+                <div class='btn btn-warning'>Who Am I?</div>\n
+                <div><strong>Who Am I?</strong> {{whoami.user}}</div>\n
+                <div>\n
+                    <input id='post-echo-input' type='text' />\n
+                    <div class='btn btn-success post-echo'>POST Echo</div>\n
+                </div>\n
+                <div>\n
+                    <strong>POST Response:</strong> {{echo.POST.result}}\n
+                </div>\n
+                <div>\n
+                    <input id='get-echo-input' type='text' />\n
+                    <div class='btn btn-success get-echo'>GET Echo</div>\n
+                </div>\n
+                <div>\n
+                <strong>GET Response:</strong> {{echo.GET.result}}\n
+                </div>\n
+                <div>{{>Test}}</div>
+                ",
+            ],
+            ],
+        ]);
         $app->save();
 
         $endpoint = new \App\Endpoint;
         $endpoint->name = 'My Endpoint';
         $endpoint->type = 'http_basic_auth';
         $endpoint->credentials = json_encode([
-            'username'=>'info','password'=>'info','url'=>'https://mercury.binghamton.edu/info/'
+            'username'=>'info','password'=>'info','url'=>'https://mercury.binghamton.edu'
         ]);
         $endpoint->site_id = $site->id;
         $endpoint->group_id = $group->id;
@@ -57,8 +136,11 @@ class DatabaseSeeder extends Seeder
         $app_instance->slug = 'my_app_instance';
         $app_instance->app_id = $app->id;
         $app_instance->group_id = $group->id;
-        $app_instance->configuration = '{"test":"hello_world"}';
-        $app_instance->resources = '[{"name": "echo","endpoint": "1","modifier": "none","cache": false,"fetch": true,"path": "echo/?data={{configuration.test}}&myrequest={{request.abc}}"},{"name": "whoami","endpoint": "1","modifier": "none","cache": false,"fetch": true,"path": "whoami/"}]';
+        $app_instance->configuration = json_encode(["test"=>"hello_world"]);
+        $app_instance->resources = json_encode([
+            ['name'=>'echo','endpoint'=>1,'modifier'=>'none','cache'=>false,'fetch'=>true,'path'=>'/info/echo/'],
+            ['name'=>'whoami','endpoint'=>1,'modifier'=>'none','cache'=>false,'fetch'=>true,'path'=>'/info/whoami/'],
+        ]);
         $app_instance->public = 0;
         $app_instance->save();
 
