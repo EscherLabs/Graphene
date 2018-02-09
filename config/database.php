d4930390ed5d9438465baaf61755bc5ac3339c60
Tim Cortesi <tcortesi@gmail.com>
2018-02-09 14:21:05 -0500

    Reverting Database Configuration
    Local DB Connection Changes should be in the local .env file.  To
    reapply changes from previous checkin, add the following to the local
    .env file:
    
    DB_HOST=127.0.0.1
    DB_USERNAME=root
    DB_PASSWORD=MyNewPass

--- config/database.php
+++ config/database.php
@@ -46,31 +46,17 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
+        // 'sqlite' => [
+        //     'driver' => 'sqlite',
+        //     'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        //     'prefix' => '',
+        // ],
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'crazystairs'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'MyNewPass'),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'mysql-portal' => [
-            'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'portal'),
+            'database' => env('DB_DATABASE', 'crazystairs'),
             'username' => env('DB_USERNAME', 'crazystairs'),
             'password' => env('DB_PASSWORD', 'crazystairs'),
             'charset' => 'utf8',
@@ -80,18 +66,32 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+        'mysql-portal' => [
+            'driver' => 'mysql',
+            'host' => env('PORTAL_DB_HOST', '127.0.0.1'),
+            'port' => env('PORTAL_DB_PORT', '3306'),
+            'database' => env('PORTAL_DB_DATABASE', 'portal'),
+            'username' => env('PORTAL_DB_USERNAME', 'crazystairs'),
+            'password' => env('PORTAL_DB_PASSWORD', 'crazystairs'),
             'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'strict' => true,
+            'engine' => null,
         ],
+
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'schema' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
 
     ],
 
@@ -119,17 +119,17 @@ return [
     |
     */
 
-    'redis' => [
+    // 'redis' => [
 
-        'cluster' => false,
+    //     'cluster' => false,
 
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
+    //     'default' => [
+    //         'host' => env('REDIS_HOST', '127.0.0.1'),
+    //         'password' => env('REDIS_PASSWORD', null),
+    //         'port' => env('REDIS_PORT', 6379),
+    //         'database' => 0,
+    //     ],
 
-    ],
+    // ],
 
 ];
