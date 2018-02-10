32e298223b4ba4be6396f1913ff77b5d3b6aacfa
Tim Cortesi <tcortesi@gmail.com>
2018-02-10 10:04:00 -0500

    Fix Page Visibility Settings

--- app/Page.php
+++ app/Page.php
@@ -9,26 +9,32 @@ class Page extends Model
     protected $fillable = ['group_id', 'name', 'slug', 'content', 'mobile_order','unlisted','public','icon','device'];
 
     protected $casts = ['content' => 'object', 'mobile_order' => 'object'];
-    protected $appends = ['visible_xs', 'visible_sm', 'visible_md', 'visible_lg'];
+    protected $appends = ['hidden_xs', 'hidden_sm', 'hidden_md', 'hidden_lg'];
+
+    /* Transient Properties not saved in the database */
+    public $hidden_xs = false;
+    public $hidden_sm = false;
+    public $hidden_md = false;
+    public $hidden_lg = false;
 
     public function group() {
       return $this->belongsTo(Group::class);
     }
 
-    public function getVisibleXsAttribute()
+    public function getHiddenXsAttribute()
     {
-        return ($this->device === 0 || $this->device === 3 || $this->device === 4);  
+        return ($this->device === 1 || $this->device === 2);  
     }
-    public function getVisibleSmAttribute()
+    public function getHiddenSmAttribute()
     {
-        return ($this->device === 0 || $this->device === 3 || $this->device === 4);  
+        return ($this->device === 1 || $this->device === 4);  
     }
-    public function getVisibleMdAttribute()
+    public function getHiddenMdAttribute()
     {
-        return ($this->device === 0 || $this->device === 2 || $this->device === 3);  
+        return ($this->device === 1 || $this->device === 4);  
     }
-    public function getVisibleLgAttribute()
+    public function getHiddenLgAttribute()
     {
-        return ($this->device === 0 || $this->device === 1 || $this->device === 2);  
+        return ($this->device === 3 || $this->device === 4);  
     }
 }
