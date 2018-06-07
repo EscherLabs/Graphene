36f8b315eafa1539e980e19058190af36c1b81c8
Tim Cortesi <tcortesi@gmail.com>
2018-06-07 11:48:02 -0400

    Replace _.contains ==> _.includes

--- database/seeds/PortalMigration.php
+++ database/seeds/PortalMigration.php
@@ -241,6 +241,7 @@ class PortalMigration extends Seeder
                     $app_db->script = str_replace('_.findWhere','_.find',$app_db->script);
                     $app_db->script = str_replace('_.filter','_.where',$app_db->script);
                     $app_db->script = str_replace('_.pluck','_.map',$app_db->script);
+                    $app_db->script = str_replace('_.contains','_.includes',$app_db->script);
 
                     $app_sources_db = json_decode($app_db->sources,true);
                     $app_sources = [];
