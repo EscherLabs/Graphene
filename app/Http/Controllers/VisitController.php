e6a439d8ded0060fa963331b8f93af7c16a4c660
Tim Cortesi <tcortesi@gmail.com>
2018-07-17 15:34:55 -0400

    Fix Log Visit for Unathenticated Users

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -17,7 +17,11 @@ class VisitController extends Controller
     public function log_visit(Request $request)
     {
         $visit = new Visit($request->all());
-        $visit->user_id = Auth::user()->id;
+        if (Auth::check()) {
+            $visit->user_id = Auth::user()->id;
+        } else {
+            $visit->user_id = 0;
+        }
         $visit->save();
         return $visit;
     }
