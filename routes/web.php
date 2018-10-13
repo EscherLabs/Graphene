3d7798c2878b952776d8b65443c5e7cf7c31c2dc
Tim Cortesi <tcortesi@gmail.com>
2018-10-13 11:17:36 -0400

    Remove extra curly brace

--- routes/web.php
+++ routes/web.php
@@ -271,7 +271,7 @@ Route::group(['middleware' => ['no.save.session'],'prefix' => 'api'], function (
     Route::get('/ellucianmobile/config','EllucianMobileController@config');
 
     Route::get('/proxy/{slug}/{route}/{object_id?}/{action?}/{selection?}','APIServerController@fetch'); // Check Permissions in Controller  
-    Route::post('/proxy/{slug}/{route}/{object_id?}/{action?/{selection?}}','APIServerController@fetch'); 
+    Route::post('/proxy/{slug}/{route}/{object_id?}/{action?/{selection?}','APIServerController@fetch'); 
     Route::put('/proxy/{slug}/{route}/{object_id?}/{action?}/{selection?}','APIServerController@fetch'); 
     Route::delete('/proxy/{slug}/{route}/{object_id?}/{action?}/{selection?}','APIServerController@fetch'); 
   });
