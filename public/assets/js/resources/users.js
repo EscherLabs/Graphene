02d8b43e376de49e9e7fdfc647cfaaf31daac5d5
Adam Smallcomb <adam@users-air.lan>
2018-10-25 22:49:26 -0400

    Add link on users for impersonate by site admin

--- public/assets/js/resources/users.js
+++ public/assets/js/resources/users.js
@@ -30,6 +30,7 @@
 				{name:'first_name',label:'First Name',enabled:false},
 				{name:'last_name',label:'Last Name',enabled:false},
 				{name:'email',label:'Email', enabled:false},
+				{name:'id',label:false,inline:true, type:'raw',template:'<div><a class="btn btn-warning pull-right" href="/api/users/{{value}}/impersonate/">Impersonate this user</a></div>'},
 				{name:'site_members',label:false, type:'fieldset',multiple: {duplicate: false},fields:[
 					{name:'site_admin',label:'Site Admin',type:'custom_radio',options:[{label:'Yes',value:1},{label:'No',value:0}]},
 					{name:'site_developer',label:'Site Developer',type:'custom_radio',options:[{label:'Yes',value:1},{label:'No',value:0}]}
@@ -38,7 +39,10 @@
 				{name:'app_developers',label:"App Developer",inline:true, type:'raw',template:'<div class="list-group" style="margin-bottom:0;max-height: 200px;overflow: auto;">{{^value.length}}None{{/value.length}}{{#value}}<div class="list-group-item" style="padding: 8px 15px;position:relative">{{app.name}}</div>{{/value}}</div>'},
 				{name:'group_admins',label:"Admin Of",inline:true, type:'raw',template:'<div class="list-group" style="margin-bottom:0;">{{^value.length}}None{{/value.length}}{{#value}}<div class="list-group-item" style="padding: 8px 15px;position:relative">{{group.name}}{{#apps_admin}}<i class="fa fa-cubes text-info" style="position:absolute;top:10px;right:10px"></i>{{/apps_admin}}{{#content_admin}}<i class="fa fa-file text-info" style="position:absolute;top:10px;right:40px"></i>{{/content_admin}}</div>{{/value}}</div>'},
 
+
 				{type:'hidden', name:'id'}
+				
+				
 			]}).on('save',function(){
 				$.ajax({
 					url: '/api/users/'+this.toJSON().id+'/info',
