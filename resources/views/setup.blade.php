9379c6ac6ac89b5b772f3030a5454c5cdfe852c8
Adam Smallcomb <atsmallcomb@gmail.com>
2019-01-17 13:02:50 -0500

    add form and template for instructions on setting up DB

--- resources/views/setup.blade.php
+++ resources/views/setup.blade.php
@@ -15,26 +15,28 @@ Guest
         <div class="panel panel-default">
             <div class="panel-heading">Graphene Setup Wizard</div>
             <div class="panel-body">
-            <div id="form"></div>
+            <div id="form">
             @if($mode == 'db')
             <h4 class="text-danger">Graphene is unable to connect to your database.</h4>
             Please make sure you have set this up in your .env file or server configuration ($_SERVER). You may need to restart the php server before changes are seen.
             <br>
             <br>
-            @if(env('APP_DEBUG') == 1)
-
+            Also make sure that your database has been configured and is reachable.
+            <br>
+            <br>
             The current configuration is:            <br>
             <br>
 
             <table class="table">
-            <tr><td style="width:120px"><b>DB_HOST</b></td><td>{{ config('database.connections.mysql.host') }}</td></tr>
-            <tr><td><b>DB_PORT</b></td><td>{{ config('database.connections.mysql.port') }}</td></tr>
-            <tr><td><b>DB_DATABASE</b></td><td>{{ config('database.connections.mysql.database') }}</td></tr>
-            <tr><td><b>DB_USERNAME</b></td><td>{{ config('database.connections.mysql.username') }}</td></tr>
-            <tr><td><b>DB_PASSWORD</b></td><td>***</span></div>
+            <tr><td style="width:120px"><b>DB_HOST</b></td><td data-inline="host"></td></tr>
+            <tr><td><b>DB_PORT</b></td><td data-inline="port"></td></tr>
+            <tr><td><b>DB_DATABASE</b></td><td data-inline="database"></td></tr>
+            <tr><td><b>DB_USERNAME</b></td><td data-inline="username"></td></tr>
+            <tr><td><b>DB_PASSWORD</b></td><td data-inline="password"></span></div>
             </table>
             @endif
-            @endif
+            </div>
+            <div id="instructions"></div>
             </div>
         </div>
     </div>
@@ -69,6 +71,39 @@ Guest
         });
         }
       })
+      @endif
+
+      @if($mode == 'db')
+
+            mysql = {};
+            @if(env('APP_DEBUG') == 1)
+
+                mysql.host = "{{ config('database.connections.mysql.host') }}";
+                mysql.port = "{{ config('database.connections.mysql.port') }}";
+                mysql.database = "{{ config('database.connections.mysql.database') }}";
+                mysql.username = "{{ config('database.connections.mysql.username') }}";
+                @if(config('database.connections.mysql.password') == 'crazystairs')
+                    mysql.password = "crazystairs";
+                @else
+                    mysql.password = "***";
+                @endif
+           @endif
+
+
+      $('#form').berry({name:'form',attributes:mysql,actions:false,renderer:'inline',fields:[
+        {name:'host'},
+        {name:'port'},
+        {name:'database'},
+        {name:'username'},
+        {name:'password'}
+        ]}).on('change',function(){
+            @verbatim
+            //generate instructions here
+			// $('#instructions').html(Hogan.compile(`hello {{host}}`).render(this.toJSON()));
+            @endverbatim
+
+        }).trigger('change')
+      
       @endif
         $('form').on('keydown', function(event) {
             try {
