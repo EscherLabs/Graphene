ab3229d7cbe053123d6471bf42cf69864d95c2b4
Tim Cortesi <tcortesi@gmail.com>
2019-11-01 08:45:43 -0400

    Back out redundant code

--- app/Http/Controllers/WorkflowSubmissionController.php
+++ app/Http/Controllers/WorkflowSubmissionController.php
@@ -183,13 +183,12 @@ class WorkflowSubmissionController extends Controller
         $state_data['assignment']['type'] = $workflow_submission->assignment_type = $m->render($state->assignment->type, $state_data);
         $state_data['assignment']['id'] = $m->render($state->assignment->id, $state_data);
 
-        $assignment_id = $m->render($state_data['assignment']['id'], $state_data);
         if($state->assignment->type == "user"){
-            $user = User::where("unique_id", '=', $assignment_id)->first();
+            $user = User::where("unique_id", '=', $state_data['assignment']['id'])->first();
             if($user !== null) {
                 $workflow_submission->assignment_id = $user->id;
             }else{
-                $user = User::where("id",'=',$assignment_id)->first();
+                $user = User::where("id",'=',$state_data['assignment']['id'])->first();
                 $workflow_submission->assignment_id = $user->id;
                 if($user === null) {
                     throw new \Exception('Assigned User Does Not Exist');
@@ -197,11 +196,11 @@ class WorkflowSubmissionController extends Controller
             }
             $state_data['assignment']['user'] = $user->only('first_name','last_name','email','unique_id','params');
         }elseif($state->assignment->type == "group"){
-            $group = Group::where("id",'=',$assignment_id)->where('site_id',config('app.site')->id)->first();
+            $group = Group::where("id",'=',$state_data['assignment']['id'])->where('site_id',config('app.site')->id)->first();
             if($group !== null) {
                 $workflow_submission->assignment_id = $group->id;
             }else{
-                $group = Group::where("slug",'=',$assignment_id)->where('site_id',config('app.site')->id)->first();
+                $group = Group::where("slug",'=',$state_data['assignment']['id'])->where('site_id',config('app.site')->id)->first();
                 $workflow_submission->assignment_id = $group->id;
                 if($group === null) {
                     throw new \Exception('Assigned Group Does Not Exist');
