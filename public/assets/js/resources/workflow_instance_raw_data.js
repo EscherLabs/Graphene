526abf2ccea0caebb55b040e4fcd897c7f7872a4
Tim Cortesi <tcortesi@gmail.com>
2020-06-26 15:03:58 -0400

    Retain State for Raw Workflow Instance Data Grid

--- public/assets/js/resources/workflow_instance_raw_data.js
+++ public/assets/js/resources/workflow_instance_raw_data.js
@@ -3,23 +3,20 @@ $('[href="/admin/workflowinstances"]').parent().addClass('active');
 
 $.ajax({
 	url: '/api/workflowinstances/'+resource_id,
-	success: function(data) {
-		$('.navbar-header .nav a h4').append(' - '+data.workflow.name+'');
-		
+	success: function(workflow_instance) {
+		$('.navbar-header .nav a h4').append(' - '+workflow_instance.workflow.name+'');
 		$.ajax({
 			url:'/api/workflowinstances/'+resource_id+'/raw',
 			dataType : 'json',
 			type: 'GET',
-
-			success  : function(data){
+			success  : function(raw_data){
 				mygrid = new GrapheneDataGrid({
 					actions:[{type:"info",name:"d_csv",label:'<i class="fa fa-download"></i> Download CSV'}],
-					autoSize:10,name:"workflow_submissions",schema:data.schema,data:data.data,download:false,upload:false,el:document.body.querySelector('#table')}).on('click',function(e){
+					autoSize:10,name:"workflow_submissions_raw_"+workflow_instance.id,schema:raw_data.schema,data:raw_data.data,download:false,upload:false,el:document.body.querySelector('#table')}).on('click',function(e){
 					document.location = "/workflows/report/"+e.model.attributes._w_id;
 				}.bind(this)).on('d_csv',function(e){
 					document.location = "/api/workflowinstances/"+resource_id+"/csv"
 				})
-				mygrid.state.set(_.extend({},mygrid.state.get(),{filters:{"_w_status":'open'}}))
 				mygrid.fixStyle();
 			}.bind(this)
 		})
