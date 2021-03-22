a686d1875cd50dbf1e68df3181a11ce1e935aa99
Tim Cortesi <tcortesi@gmail.com>
2021-03-22 12:19:56 -0400

    Submit / Update Workflow (Untested)

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,8 @@ Route::group(['middleware' => ['public.api.auth', 'no.save.session'], 'prefix' =
     Route::get('/workflows/submissions/{submission}','PublicAPIWorkflowController@get_submission'); 
     // Return all workflow submissions by instance_id
     Route::get('/workflows/instances/{instance}/submissions','PublicAPIWorkflowController@get_all_instance_submissions');
+    // Submit a new workflow submission on behalf of user (unique_id), given a workflow instance_id
+    Route::post('/workflows/instances/{workflow_instance}/user/{unique_id}','WorkflowSubmissionActionController@api_create');
+    // Update an existing workflow_submission on behalf of user (unique_id), given a workflow sumbission_id
+    Route::put('/workflows/submissions/{workflow_submission}/user/{unique_id}','WorkflowSubmissionActionController@api_action');
 });
