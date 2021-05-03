886b76c21cd09123024bf404ddc34bf583792b3a
Tim Cortesi <tcortesi@gmail.com>
2021-05-03 14:01:53 -0400

    Remove health check /* wildcard

--- app/Http/Middleware/Initialization.php
+++ app/Http/Middleware/Initialization.php
@@ -22,7 +22,7 @@ class Initialization
     public function handle($request, Closure $next)
     {
         // Don't worry about the site if we're just doing a simple health check
-        if ($request->is('health/*')) {
+        if ($request->is('health')) {
             // TJC This is not great, and should be fixed, but leaving it here for now.
             // (This bypasses everything else and just returns OK.  We should let this call a controller with a proper health check)
             return response('OK',200)->header('Content-Type', 'text/plain');
