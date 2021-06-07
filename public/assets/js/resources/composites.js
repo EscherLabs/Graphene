407c0b648a53563211154200eb29b459ec0415f8
Adam Smallcomb <atsmallcomb@gmail.com>
2021-06-07 11:04:20 -0400

    fix composites table display

--- public/assets/js/resources/composites.js
+++ public/assets/js/resources/composites.js
@@ -3,10 +3,9 @@ $('[href="/admin/groups"]').parent().addClass('active');
 
 getData('/api/groups/'+resource_id+'/composites', composites => {
 	routes.create = routes.delete = '/api/groups/{{resource_id}}/composites/{{id}}';
-
 	new GrapheneDataGrid({...tableConfig,
 		schema: [
-			{label: 'Group', name:'id', required: true, type:'group'}
+			{label: 'Group', name:'id', required: true, type:'group',template:"{{attributes.name}}"}
 		],
 		actions: [
 			{'name':'delete'},'|','|',
