c0a7ba333ada43a7cf950805d644784072d80d9d
Adam Smallcomb <atsmallcomb@gmail.com>
2022-07-20 17:02:11 -0400

    clean dirty data on appEdit

--- public/assets/js/fileManager.js
+++ public/assets/js/fileManager.js
@@ -1,4 +1,3 @@
-
 // gform.stencils.ace = `
 // <div class="row clearfix form-group" name="{{name}}">
 // 	{{>_label}}
@@ -53,13 +52,13 @@
 //     this.editor.setTheme(this.item.theme || "ace/theme/chrome");
 //     this.editor.getSession().setMode({path: this.owner.options.default.mode || this.item.mode || "ace/mode/handlebars", inline:this.owner.options.default.inlinemode || this.item.inlinemode});
 //     this.editor.session.setValue(this.value);
-   
+
 //   },
 //   // update: function(item, silent) {
 //   //   if(typeof item !== 'undefined' && (
 //   //       typeof item.options !== undefined ||
 //   //       typeof item.max !== undefined ||
-//   //       typeof item.action !== undefined 
+//   //       typeof item.action !== undefined
 //   //       )
 //   //       && typeof this.mapOptions !== 'undefined'){
 //   //       delete this.mapOptions;
@@ -92,7 +91,7 @@
 //   //       this.owner.pub(['change:'+this.name,'change'], this);
 //   //   }
 //   //   if(typeof gform.types[this.type].setup == 'function') {gform.types[this.type].setup.call(this);}
-    
+
 //   // },
 //   set:function(value){
 //     this.editor.session.setValue(value);
@@ -105,123 +104,209 @@
 //   }
 // });
 
-var fileManager = function(selector, options){
+var fileManager = function (selector, options) {
   this.$el = $(selector);
-  options.hasextra  = (typeof options.extra == 'function');
-  options.u_id =  gform.getUID();
-  options.label = options.label || 'Section';
-  options.items = _.map(options.items, function(item) {
-    item.key = options.u_id+item.name.toLowerCase().replace(/ /g,"_").split('.').join('_');
+  options.hasextra = typeof options.extra == "function";
+  options.u_id = gform.getUID();
+  options.label = options.label || "Section";
+  options.items = _.map(options.items, function (item) {
+    item.key =
+      options.u_id +
+      item.name.toLowerCase().replace(/ /g, "_").split(".").join("_");
     return item;
-  })
-  options.fields = _.map(options.items, function(item) {
-        return {target:'#'+item.key,name:item.key};        
-  })  
+  });
+  options.fields = _.map(options.items, function (item) {
+    return { target: "#" + item.key, name: item.key };
+  });
   options.actions = [];
   options.clear = false;
-  options.data  = {};
-  _.each(options.items, function(item) {
-    if(typeof item.content == 'object'){
-      item.content = JSON.stringify(item.content)
+  options.data = {};
+  _.each(options.items, function (item) {
+    if (typeof item.content == "object") {
+      item.content = JSON.stringify(item.content);
     }
     options.data[item.key] = item.content;
-  })
-  options.default ={label: false,type:'ace',mode:options.mode || 'ace/mode/handlebars', inlinemode:options.inlinemode}
-  this.options = $.extend(true,{editable: true},options);
+  });
+  options.default = {
+    label: false,
+    type: "ace",
+    mode: options.mode || "ace/mode/handlebars",
+    inlinemode: options.inlinemode,
+  };
+  this.options = $.extend(true, { editable: true }, options);
   this.active = this.options.items[0].key;
-  $(selector).html(templates.pages.render(this.options,templates));
-  this.gform = new gform(this.options,selector);
-  this.render = function(){
-    $(selector+' .list-group').empty().html(templates.pages_listgroupitem.render(this.options));
-    $('[href="#'+this.active+'"]').click();
-  }
-  this.sorter = new Sortable(document.querySelector(selector+' .list-group'),{draggable:'.sortable'})
+  $(selector).html(templates.pages.render(this.options, templates));
+  this.gform = new gform(this.options, selector);
 
-  $(selector+' .actions .pages_delete,'+selector+' .actions .pages_edit,'+selector+' .actions .pages_new,'+selector+' .pages_extra').on('click', function(e){
-    var currentItem = _.findWhere(this.options.items, {key: this.active});
-    if($(e.currentTarget).hasClass('pages_delete') && !currentItem.disabled){
-      currentItem.removed = true;
-      this.render();
-    }else{
-      if($(e.currentTarget).hasClass('pages_edit') && !currentItem.disabled){
+  this.render = function () {
+    $(selector + " .list-group")
+      .empty()
+      .html(templates.pages_listgroupitem.render(this.options));
+    $('[href="#' + this.active + '"]').click();
+  };
+  this.sorter = new Sortable(
+    document.querySelector(selector + " .list-group"),
+    { draggable: ".sortable" }
+  );
 
-        new gform({name:'page_name', legend: 'Edit '+options.label, data: {name: currentItem.name},fields: [{label:'Name'}]} ).on('save', function(e){
-          _.findWhere(this.options.items, {key:this.active}).name = e.form.get().name;
-          this.render();
-          e.form.dispatch('close');
-        }.bind(this)).on('cancel',function(e){e.form.dispatch('close')}).modal();
-      }else{
-        if($(e.currentTarget).hasClass('pages_new')){
-          new gform({name:'page_name', legend: 'New '+options.label,fields: [{label:'Name'}]}).on('save', function(e){
-            this.add(e.form.get().name,'')
-            e.form.dispatch('close');
-          }.bind(this)).on('cancel',function(e){e.form.dispatch('close')}).modal();
-        }else{
-          if($(e.currentTarget).hasClass('pages_extra')) {
-            this.options.extra.call(this, currentItem);
-          }else{
-          	if(currentItem.disabled){
-          		toastr.error('This action is disabled for this item','Disabled')
-          	}
+  $(
+    selector +
+      " .actions .pages_delete," +
+      selector +
+      " .actions .pages_edit," +
+      selector +
+      " .actions .pages_new," +
+      selector +
+      " .pages_extra"
+  ).on(
+    "click",
+    function (e) {
+      var currentItem = _.findWhere(this.options.items, { key: this.active });
+      if (
+        $(e.currentTarget).hasClass("pages_delete") &&
+        !currentItem.disabled
+      ) {
+        currentItem.removed = true;
+        this.render();
+      } else {
+        if (
+          $(e.currentTarget).hasClass("pages_edit") &&
+          !currentItem.disabled
+        ) {
+          new gform({
+            name: "page_name",
+            legend: "Edit " + options.label,
+            data: { name: currentItem.name },
+            fields: [{ label: "Name" }],
+          })
+            .on(
+              "save",
+              function (e) {
+                _.findWhere(this.options.items, { key: this.active }).name =
+                  e.form.get().name;
+                this.render();
+                e.form.dispatch("close");
+              }.bind(this)
+            )
+            .on("cancel", function (e) {
+              e.form.dispatch("close");
+            })
+            .modal();
+        } else {
+          if ($(e.currentTarget).hasClass("pages_new")) {
+            new gform({
+              name: "page_name",
+              legend: "New " + options.label,
+              fields: [{ label: "Name" }],
+            })
+              .on(
+                "save",
+                function (e) {
+                  this.add(e.form.get().name, "");
+                  e.form.dispatch("close");
+                }.bind(this)
+              )
+              .on("cancel", function (e) {
+                e.form.dispatch("close");
+              })
+              .modal();
+          } else {
+            if ($(e.currentTarget).hasClass("pages_extra")) {
+              this.options.extra.call(this, currentItem);
+            } else {
+              if (currentItem.disabled) {
+                toastr.error(
+                  "This action is disabled for this item",
+                  "Disabled"
+                );
+              }
+            }
           }
         }
       }
-    }
-  }.bind(this));
+    }.bind(this)
+  );
 
-  $(selector).on('click','.list-group-item.tab',function(e){
-    $(e.currentTarget).parent().find('.list-group-item').removeClass('active');
-    $(e.currentTarget).addClass('active');
-    this.active = $(e.currentTarget).attr('aria-controls');
-		$(e.currentTarget).parent().parent().find('button.dropdown-toggle').prop('disabled', 
-		  (_.findWhere(this.options.items, {key: this.active}) || this.options.items[0]).disabled || false
-		);
-    if(typeof this.gform.find(this.active) !== 'undefined'){
-      this.gform.find(this.active).editor.clearSelection();//.instances[0]
-      gform.types['ace'].focus.call(this.gform.find(this.active))
-
-    }
-  }.bind(this))
-  $(selector).find('.list-group-item.tab').first().click();
-  this.getCurrent = function(){
-      return _.findWhere(this.options.items, {key: this.active});
-  }
-  this.add = function(name, value){
-    var key = this.options.u_id+name.toLowerCase().replace(/ /g,"_").split('.').join('_');;
-    this.options.items.push({name: name,key:key, content:""})
-    this.$el.find('.tab-content').append(templates.pages_tabpanel.render({name: name,key:key, content:""}));
-    if(typeof this.gform.find(key) == 'undefined'){
+  $(selector).on(
+    "click",
+    ".list-group-item.tab",
+    function (e) {
+      $(e.currentTarget)
+        .parent()
+        .find(".list-group-item")
+        .removeClass("active");
+      $(e.currentTarget).addClass("active");
+      this.active = $(e.currentTarget).attr("aria-controls");
+      $(e.currentTarget)
+        .parent()
+        .parent()
+        .find("button.dropdown-toggle")
+        .prop(
+          "disabled",
+          (
+            _.findWhere(this.options.items, { key: this.active }) ||
+            this.options.items[0]
+          ).disabled || false
+        );
+      if (typeof this.gform.find(this.active) !== "undefined") {
+        this.gform.find(this.active).editor.clearSelection(); //.instances[0]
+        gform.types["ace"].focus.call(this.gform.find(this.active));
+      }
+    }.bind(this)
+  );
+  $(selector).find(".list-group-item.tab").first().click();
+  this.getCurrent = function () {
+    return _.findWhere(this.options.items, { key: this.active });
+  };
+  this.add = function (name, value) {
+    var key =
+      this.options.u_id +
+      name.toLowerCase().replace(/ /g, "_").split(".").join("_");
+    this.options.items.push({ name: name, key: key, content: "" });
+    this.$el
+      .find(".tab-content")
+      .append(
+        templates.pages_tabpanel.render({ name: name, key: key, content: "" })
+      );
+    if (typeof this.gform.find(key) == "undefined") {
       // this.gform.fields.push(gform.createField.call(this.gform, this.gform, {}, null ,null, $.extend({name:key,target:'#'+key},this.gform.options.default)))
-      this.gform.add({name:key,target:'#'+key})
-    }else{      
-      var updateItem = _.findWhere(this.options.items, {name: name});
-      
+      this.gform.add({ name: key, target: "#" + key });
+    } else {
+      var updateItem = _.findWhere(this.options.items, { name: name });
+
       updateItem.removed = false;
     }
     this.render();
-    this.gform.find(key).set(value)
-  }
-  return {
-    toJSON:function(){
+    this.gform.find(key).set(value);
+  };
+  this.response = {
+    toJSON: function () {
       var options = this.options;
 
       var data = this.gform.toJSON();
-      var order = _.map($(selector+' .list-group').children(),function(item){return $(item).attr('name')})
+      var order = _.map(
+        $(selector + " .list-group").children(),
+        function (item) {
+          return $(item).attr("name");
+        }
+      );
 
-      var temp = _.map(order,function(item){
-      var cachedItem = _.findWhere(options.items, {key:item});
-        
-        if(typeof cachedItem !== 'undefined' && !cachedItem.removed){
-          return {name: _.findWhere(options.items, {key:item}).name, content: data[item]};
-        }else{
+      var temp = _.map(order, function (item) {
+        var cachedItem = _.findWhere(options.items, { key: item });
+
+        if (typeof cachedItem !== "undefined" && !cachedItem.removed) {
+          return {
+            name: _.findWhere(options.items, { key: item }).name,
+            content: data[item],
+          };
+        } else {
           return false;
         }
       });
 
-
       // var temp = _.map(this.gform.toJSON(),function(item,i){
       //   var cachedItem = _.findWhere(options.items, {key:i});
-        
+
       //   if(typeof cachedItem !== 'undefined' && !cachedItem.removed){
       //     return {name: _.findWhere(options.items, {key:i}).name, content: item};
       //   }else{
@@ -229,43 +314,77 @@ var fileManager = function(selector, options){
       //   }
       // });
 
-
-
-      return _.filter(temp, function(item){return item;});
+      return _.filter(temp, function (item) {
+        return item;
+      });
     }.bind(this),
-    getCurrent:this.getCurrent.bind(this),
-    update:function(key,value){
-      this.gform.find(key).set(value)
+    getCurrent: this.getCurrent.bind(this),
+    update: function (key, value) {
+      if (this.gform.find(key).get() !== value) {
+        this.gform.find(key).set(value);
+      }
     }.bind(this),
-    remove: function(name){
-      var updateItem = _.findWhere(this.options.items, {name: name});
-      this.options.items.splice(_.indexOf(this.options.items, updateItem),1)
+    remove: function (name) {
+      var updateItem = _.findWhere(this.options.items, { name: name });
+      this.options.items.splice(_.indexOf(this.options.items, updateItem), 1);
       // updateItem.removed = true;
-      this.render();      
+      this.render();
     }.bind(this),
     add: this.add.bind(this),
-    errors: function(){
-    	// var errors = 0;
-    	var errors = [];
-    	_.each(this.options.items, function(item){
-    		var items = _.where(this.gform.find(item.key).editor.session.getAnnotations(), {type:"error"});
-    		for(var i in items){
-    			items[i].name = item.name;
-    		}
-				errors = errors.concat(items);
+    errors: function () {
+      // var errors = 0;
+      var errors = [];
+      _.each(
+        this.options.items,
+        function (item) {
+          var items = _.where(
+            this.gform.find(item.key).editor.session.getAnnotations(),
+            { type: "error" }
+          );
+          for (var i in items) {
+            items[i].name = item.name;
+          }
+          errors = errors.concat(items);
 
-    		var content = item.name;
+          var content = item.name;
 
-    		if(items.length){
-    			content = '<span class="badge badge-danger">'+items.length+ '</span> '+ item.name;
-				}
-    		if(item.disabled){
-    			content = '<i class="fa fa-ban"></i> ' +content;
-    		}
-				this.$el.find('.list-group-item.tab[name="'+item.key+'"]').html(content);
-    	}.bind(this))
+          if (items.length) {
+            content =
+              '<span class="badge badge-danger">' +
+              items.length +
+              "</span> " +
+              item.name;
+          }
+          if (item.disabled) {
+            content = '<i class="fa fa-ban"></i> ' + content;
+          }
+          this.$el
+            .find('.list-group-item.tab[name="' + item.key + '"]')
+            .html(content);
+        }.bind(this)
+      );
 
-    	return errors;
-    }.bind(this)
-  }
-}
+      return errors;
+    }.bind(this),
+  };
+  Object.defineProperty(this.response, "isDirty", {
+    get: () => {
+      return !_.every(
+        this.response.toJSON(),
+        (item, key) =>
+          item.name == this.options.items[key].name &&
+          _.isEqual(item.content, this.options.items[key].content)
+      );
+    },
+    set: status => {
+      if (!status && status !== this.response.isDirty) {
+        console.log("Clean");
+        _.each(this.response.toJSON(), (item, key) => {
+          this.options.items[key].name = item.name;
+          this.options.items[key].content = item.content;
+        });
+      }
+    },
+  });
+  return this.response;
+};
